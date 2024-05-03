8f3ec1f68d2f611df34e769d3e18cb79989615a0
Kevin <kevin_jeon@icloud.com>
2024-05-03 11:46:45 -0400

    created xcode project

---
+++ StepTracker-SeanAllen/StepTracker_SeanAllenApp.swift
@@ -0,0 +1,17 @@
+//
+//  StepTracker_SeanAllenApp.swift
+//  StepTracker-SeanAllen
+//
+//  Created by Kevin Mattocks on 5/3/24.
+//
+
+import SwiftUI
+
+@main
+struct StepTracker_SeanAllenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
