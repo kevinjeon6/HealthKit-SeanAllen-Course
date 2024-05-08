96a407b57c062154a1f0b8712cf6c5c598fb699f
Kevin <kevin_jeon@icloud.com>
2024-05-08 12:42:19 -0400

    Add interactivity to step bar chart

--- StepTracker-SeanAllen/DashboardView.swift
+++ StepTracker-SeanAllen/DashboardView.swift
@@ -34,6 +34,7 @@ struct DashboardView: View {
     @AppStorage("hasSeenPermissionView") private var hasSeenPermissionView = false
     @State private var isShowingHealthKitPermissionSheet = false
     @State private var selectedStat: HealthMetricContext = .steps
+    @State private var rawSelectedDate: Date?
     
     ///Computed property to make things a bit neater for the tint modifier based on steps or weight
     ///If didn't have computed property, you would have selectedStat == .steps ? .mint : .indigo
@@ -46,6 +47,11 @@ struct DashboardView: View {
         
         let totalSteps = hkManager.stepData.reduce(0) {$0 + $1.value}
         return totalSteps/Double(hkManager.stepData.count)
+    }
+    
+    var selectedHealthMetric: HealthMetric? {
+        guard let rawSelectedDate else { return nil }
+        return hkManager.stepData.first { Calendar.current.isDate(rawSelectedDate, inSameDayAs: $0.date) }
     }
     
     // MARK: - Body
@@ -84,6 +90,23 @@ struct DashboardView: View {
                         
                        // MARK: - Chart
                         Chart {
+                            if let selectedHealthMetric {
+                                RuleMark(x: .value("Selected Metric", selectedHealthMetric.date, unit: .day))
+                                    .foregroundStyle(Color.secondary.opacity(0.3))
+                                    .offset(y: -10)
+                                    .zIndex(-1)
+                                    .annotation(
+                                        position: .top,
+                                        spacing: 0,
+                                        overflowResolution: .init(
+                                            x: .fit(to: .chart),
+                                            y: .disabled
+                                        )
+                                    ) {
+                                        annotationView
+                                    }
+                            }
+                            
                             RuleMark(y: .value("Average", avgStepCount))
                                 .foregroundStyle(.secondary)
                                 .lineStyle(.init(lineWidth: 1, dash: [5]))
@@ -94,10 +117,12 @@ struct DashboardView: View {
                                     y: .value("Steps", steps.value)
                                 )
                                 .foregroundStyle(Color.mint.gradient)
+                                .opacity(rawSelectedDate == nil || steps.date == selectedHealthMetric?.date ? 1.0 : 0.3 )
                                 
                             }
                         }
                         .frame(height: 150)
+                        .chartXSelection(value: $rawSelectedDate.animation(.easeInOut))
                         .chartXAxis {
                             AxisMarks {
                                 AxisValueLabel(format: .dateTime.month(.defaultDigits).day())
@@ -158,6 +183,26 @@ struct DashboardView: View {
         }
         .tint(isSteps ? .mint : .indigo)
     }
+    
+    
+    var annotationView: some View {
+        VStack(alignment: .leading) {
+            Text(selectedHealthMetric?.date ?? .now, format: .dateTime.weekday(.abbreviated).month(.abbreviated).day())
+                .font(.footnote.bold())
+                .foregroundStyle(.secondary)
+            
+            Text(selectedHealthMetric?.value ?? 0, format: .number.precision(.fractionLength(0)))
+                .fontWeight(.heavy)
+                .foregroundStyle(.mint)
+        }
+        .padding(12)
+        .background(
+            RoundedRectangle( cornerRadius: 4)
+                .fill(Color(.secondarySystemBackground))
+                .shadow(color: .secondary.opacity(0.3), radius: 2, x: 2, y: 2)
+        )
+    }
+    
 }
 
 #Preview {
